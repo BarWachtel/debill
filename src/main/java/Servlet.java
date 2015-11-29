752eef24994e86fbbd6515c43701937948a89c8b
ytmlndr <ytmlndr@gmail.com>
2015-11-29 16:46:26 +0200

    pom.xml - update servlet version dependency

--- src/main/java/Servlet.java
+++ src/main/java/Servlet.java
@@ -6,9 +6,6 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-/**
- * Created by user on 29/11/2015.
- */
 @WebServlet(name = "test", urlPatterns = {"/test"})
 public class Servlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
@@ -18,7 +15,7 @@ public class Servlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-			PrintWriter resp = response.getWriter();
+		PrintWriter resp = response.getWriter();
 		resp.print("suck mah booolz");
 		resp.flush();
 	}
