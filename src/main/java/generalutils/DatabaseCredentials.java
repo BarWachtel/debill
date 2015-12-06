350735a8e12ce86457dbe31110cc917d0e31ba00
BarWachtel <barwachtel@gmail.com>
2015-12-06 22:38:02 +0200

    Delete DatabaseCredentials.java

--- src/main/java/generalutils/DatabaseCredentials.java
+++
@@ -1,7 +0,0 @@
-package generalutils;
-
-public class DatabaseCredentials {
-	public static final String DB_NAME = "debill";
-	public static final String USER_NAME = "bazza";
-	public static final String PASSWORD = "bazza";
-}
